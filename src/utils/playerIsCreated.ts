ac21081a746d0081b9b21d163c8147ad712a2c88
Rypin <hhgfcswd@gmail.com>
2025-08-06 14:53:19 +0300

    delete unused file

--- src/utils/playerIsCreated.ts
+++
@@ -1,9 +0,0 @@
-import {getState} from "../state/state.ts";
-import {Eho} from "../commands/system/eho.ts";
-
-export async function playerIsCreated () {
-    if(getState('flags').playerIsCreated) {
-        await Eho('Персонаж еще не создан. Команда невозможна', 'error')
-        throw Error()
-    }
-}
