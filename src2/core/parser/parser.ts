539309f4e7b8885b9df951da5f25085865843f56
Rypin <hhgfcswd@gmail.com>
2025-08-15 13:10:42 +0300

    fix: убрал возможность отправлять команды, пока выполняется другая

--- src2/core/parser/parser.ts
+++ src2/core/parser/parser.ts
@@ -4,16 +4,19 @@ import {executeCommand} from "./executor";
 import {saveCommand} from "../history";
 import {updateState} from "../state";
 import {write} from "../cli";
+import {checkFlag} from "../utils";
 
 export async function parse(input: string): Promise<void> {
     saveCommand(input)
     updateState('cli', {input: []})
+    await checkFlag('canSendCommand', false)
+    updateState('flags', {canSendCommand: false})
     const parsedCommand = tokenize(input)
     const args = parsedCommand.args
     const command = validateCommand(parsedCommand)
-    if(command.valid && command.entry) {
-        await write('\n', 'default', [0,0])
+    if (command.valid && command.entry) {
+        await write('\n', 'default', [0, 0])
         await executeCommand(command.entry, args)
-    }
-    else console.warn(command.error)
+    } else console.warn(command.error)
+    updateState('flags', {canSendCommand: true})
 }
