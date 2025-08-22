5c0d545f91df2d603aa1adbda8d71407302b43df
Rypin <hhgfcswd@gmail.com>
2025-08-22 19:18:08 +0300

    fix: пофиксил пересчет максимального здоровья игрока
    - если экипировать два предмета, один из которых не изменяет силу, то здоровья станет больше.

--- src/commands/player/local/updateStats.ts
+++ src/commands/player/local/updateStats.ts
@@ -4,7 +4,7 @@ import {calcPlayerNewDef} from "../../../calculations/calcPlayerNewDef";
 
 export function updateStats(changeCurrentHp = true) {
     const player = getState('player')!
-    const newMaxHp = calcPlayerNewMaxHp(player.spec, player.stats.strength) + player.hp.max
+    const newMaxHp = calcPlayerNewMaxHp(player.spec, player.stats.strength)
     const newDef = calcPlayerNewDef(player.spec, player.stats.strength) + player.def
     updateState('player', {
         hp: {max: newMaxHp, current: changeCurrentHp ? newMaxHp : player.hp.current},
